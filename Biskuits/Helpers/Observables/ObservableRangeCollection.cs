6e2a448bd50b8a5a4a98a88dd296e69b6bd943d7
ppn <ponponner@gmail.com>
2018-08-02 21:36:53 +0900

    [Biskuits][追加] ObservableRangeCollection を追加およびテストした。

---
+++ Biskuits/Helpers/Observables/ObservableRangeCollection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Biskuits.Helpers.Observables
+{
+    /// <summary> 
+    /// このクラスは動的なデータコレクションを表し、項目が追加された時、削除された時、
+    /// またはリスト全体がリフレッシュされた時、その旨を通知します。
+    /// </summary> 
+    /// <typeparam name="T">要素のタイプを表します。</typeparam> 
+    public class ObservableRangeCollection<T> : ObservableCollection<T>
+    {
+        public ObservableRangeCollection()
+            : base()
+        {
+        }
+
+        /// <exception cref="System.ArgumentNullException" />
+        public ObservableRangeCollection(IEnumerable<T> collection)
+            : base(collection)
+        {
+        }
+
+        /// <exception cref="System.ArgumentNullException" />
+        public void AddRange(IEnumerable<T> collection, NotifyCollectionChangedAction notificationMode = NotifyCollectionChangedAction.Add)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            if (collection.Count() == 0)
+                return;
+
+            CheckReentrancy();
+
+            if (notificationMode == NotifyCollectionChangedAction.Reset)
+            {
+                foreach (var i in collection)
+                {
+                    Items.Add(i);
+                }
+
+                OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+                OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+                return;
+            }
+
+            int startIndex = Count;
+            var changedItems = collection is ObservableRangeCollection<T> ? (ObservableRangeCollection<T>)collection : new ObservableRangeCollection<T>(collection);
+            foreach (var i in changedItems)
+            {
+                Items.Add(i);
+            }
+
+            OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, changedItems, startIndex));
+        }
+
+        /// <exception cref="System.ArgumentNullException" />
+        public void RemoveRange(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            if (collection.Count() == 0)
+                return;
+
+            foreach (var i in collection)
+                Items.Remove(i);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        /// <exception cref="System.ArgumentNullException" />
+        public void ReplaceAll(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            Items.Clear();
+
+            //if (collection.Count() == 0)
+            //    return;
+
+            foreach (var i in collection)
+                Items.Add(i);
+
+            OnPropertyChanged(new PropertyChangedEventArgs("Count"));
+            OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
---
+++ UnitTests/Helpers/Observables/ObservableRangeCollectionTest.cs
@@ -0,0 +1,142 @@
+﻿using Biskuits.Helpers.Observables;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace UnitTests.Helpers.ObservableCollections
+{
+    [TestFixture]
+    class ObservableRangeCollectionTest
+    {
+        [Test]
+        public void TestNew_WithArgument()
+        {
+            var collection = new List<int>() { 1, 2, 3 };
+            var orc = new ObservableRangeCollection<int>(collection);
+
+            Assert.That(orc, Is.EqualTo(collection));
+        }
+
+        [Test]
+        public void TestAddRange_Reset()
+        {
+            var collection_01 = new List<int>() { 1, 2, 3 };
+            var collection_02 = new List<int>() { 4, 5 };
+            var collection_marged = new List<int>();
+            collection_marged.AddRange(collection_01);
+            collection_marged.AddRange(collection_02);
+
+            var orc = new ObservableRangeCollection<int>(collection_01);
+            var isEventOccurred = false;
+            orc.CollectionChanged += (sender, e) =>
+            {
+                isEventOccurred = true;
+                Assert.That(e.Action, Is.EqualTo(NotifyCollectionChangedAction.Reset));
+            };
+
+            orc.AddRange(collection_02, NotifyCollectionChangedAction.Reset);
+            Assert.That(orc, Is.EqualTo(collection_marged));
+            Assert.That(isEventOccurred);
+        }
+
+        [Test]
+        public void TestAddRange()
+        {
+            var collection_01 = new List<int>() { 1, 2, 3 };
+            var collection_02 = new List<int>() { 4, 5 };
+            var collection_marged = new List<int>();
+            collection_marged.AddRange(collection_01);
+            collection_marged.AddRange(collection_02);
+
+            var orc = new ObservableRangeCollection<int>(collection_01);
+            var orcCount = orc.Count;
+            var isEventOccurred = false;
+            orc.CollectionChanged += (sender, e) =>
+            {
+                isEventOccurred = true;
+                Assert.That(e.Action, Is.EqualTo(NotifyCollectionChangedAction.Add));
+                Assert.That(e.NewItems, Is.EqualTo(collection_02));
+                Assert.That(e.NewStartingIndex, Is.EqualTo(orcCount));
+                Assert.That(e.OldItems, Is.EqualTo(null));
+                Assert.That(e.OldStartingIndex, Is.EqualTo(-1));
+            };
+
+            orc.AddRange(collection_02);
+            Assert.That(orc, Is.EqualTo(collection_marged));
+            Assert.That(isEventOccurred);
+        }
+
+        [Test]
+        public void TestRemoveRange()
+        {
+            var collection_01 = new List<int>() { 1, 3, 5 };
+            var collection_02 = new List<int>() { 4, 2 };
+            var collection_marged = new List<int>();
+            collection_marged.AddRange(collection_01);
+            collection_marged.AddRange(collection_02);
+
+            var orc = new ObservableRangeCollection<int>(collection_marged);
+            var isEventOccurred = false;
+            orc.CollectionChanged += (sender, e) =>
+            {
+                isEventOccurred = true;
+                Assert.That(e.Action, Is.EqualTo(NotifyCollectionChangedAction.Reset));
+                Assert.That(e.NewItems, Is.EqualTo(null));
+                Assert.That(e.NewStartingIndex, Is.EqualTo(-1));
+                Assert.That(e.OldItems, Is.EqualTo(null));
+                Assert.That(e.OldStartingIndex, Is.EqualTo(-1));
+            };
+
+            orc.RemoveRange(collection_02);
+            Assert.That(orc, Is.EqualTo(collection_01));
+            Assert.That(isEventOccurred);
+        }
+
+        [Test]
+        public void TestReplaceAll_01()
+        {
+            var collection_01 = new List<int>() { 1, 2, 3 };
+            var collection_02 = new List<int>() { 4, 5 };
+
+            var orc = new ObservableRangeCollection<int>(collection_01);
+            var isEventOccurred = false;
+            orc.CollectionChanged += (sender, e) =>
+            {
+                isEventOccurred = true;
+                Assert.That(e.Action, Is.EqualTo(NotifyCollectionChangedAction.Reset));
+                Assert.That(e.NewItems, Is.EqualTo(null));
+                Assert.That(e.NewStartingIndex, Is.EqualTo(-1));
+                Assert.That(e.OldItems, Is.EqualTo(null));
+                Assert.That(e.OldStartingIndex, Is.EqualTo(-1));
+            };
+
+            orc.ReplaceAll(collection_02);
+            Assert.That(orc, Is.EqualTo(collection_02));
+            Assert.That(isEventOccurred);
+        }
+
+        [Test]
+        public void TestReplaceAll_02()
+        {
+            var collection_01 = new List<int>() { 1, 2, 3 };
+            var collection_02 = new List<int>() {};
+
+            var orc = new ObservableRangeCollection<int>(collection_01);
+            var isEventOccurred = false;
+            orc.CollectionChanged += (sender, e) =>
+            {
+                isEventOccurred = true;
+                Assert.That(e.Action, Is.EqualTo(NotifyCollectionChangedAction.Reset));
+                Assert.That(e.NewItems, Is.EqualTo(null));
+                Assert.That(e.NewStartingIndex, Is.EqualTo(-1));
+                Assert.That(e.OldItems, Is.EqualTo(null));
+                Assert.That(e.OldStartingIndex, Is.EqualTo(-1));
+            };
+
+            orc.ReplaceAll(collection_02);
+            Assert.That(orc, Is.EqualTo(collection_02));
+            Assert.That(isEventOccurred);
+        }
+
+    }
+}
